8b59225659fd0d8ffe8b1df3b281f10a1a03b8c8
Zoe Kavanagh <zoekavanagh2017@hotmail.com>
2018-08-01 12:12:54 +0100

    Change tests to reflect name change of method for frame_type to status and add rolls for each test to allow for accurate testing of changed status method

--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -1,20 +1,22 @@
 require 'frame'
 
 describe Frame do
-  describe '#frame_type' do
+  describe '#status' do
     it 'should return strike if first roll in frame is 10' do
-      rolls = [10]
-      expect(subject.frame_type(rolls)).to eq 'strike'
+      subject.add_roll(10)
+      expect(subject.status).to eq 'strike'
     end
 
     it 'should return spare if both rolls total 10' do
-      rolls = [8, 2]
-      expect(subject.frame_type(rolls)).to eq 'spare'
+      subject.add_roll(8)
+      subject.add_roll(2)
+      expect(subject.status).to eq 'spare'
     end
 
     it 'should return normal if total for both rolls are less than 10' do
-      rolls = [5, 3]
-      expect(subject.frame_type(rolls)).to eq 'normal'
+      subject.add_roll(2)
+      subject.add_roll(3)
+      expect(subject.status).to eq 'complete'
     end
   end
 
