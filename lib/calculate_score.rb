5c7abf406759d5fd8f1539ed98bb293d2e137966
Zoe Kavanagh <zoekavanagh2017@hotmail.com>
2018-08-01 15:40:03 +0100

    Refactor calculate_special_score method to call on specific spare_score and strike_score methods.  Remove the return of Game finished from calculate_special_score. Add additional score of 2nd frame for strike

--- lib/calculate_score.rb
+++ lib/calculate_score.rb
@@ -4,7 +4,7 @@ class Calculate_score
     @total_score = 0
   end
 
-  def score_calc(frames)
+  def calculation(frames)
     @frames = frames
     @frames.each_with_index { |frame, index| calculate_frame_score(frame, index) }
     @total_score
@@ -13,17 +13,30 @@ class Calculate_score
 private
 
   def calculate_frame_score(frame, frame_index)
-      @total_score += frame.rolls.sum
-      calculate_special_scores(frame, frame_index)
+    @total_score += frame.rolls.sum
+    calculate_special_scores(frame, frame_index)
   end
 
   def calculate_special_scores(frame, frame_index)
-    if @frames[frame_index + 1].nil?
-      return 'Game Finished'
-    elsif frame.type == 'spare'
+    if @frames[frame_index + 1].nil? == false
+      spare_score(frame, frame_index)
+      strike_score(frame, frame_index)
+    end
+  end
+
+  def spare_score(frame, frame_index)
+    if frame.status == 'spare' || frame.status == 'strike'
       @total_score += @frames[frame_index + 1].rolls[0]
-    elsif frame.type == 'strike'
-      @total_score += @frames[frame_index + 1].rolls.sum
+    end
+  end
+
+  def strike_score(frame, frame_index)
+    if frame.status == 'strike'
+      if @frames[frame_index + 1].rolls[1].nil? == false
+        @total_score += @frames[frame_index + 1].rolls[1]
+      elsif @frames[frame_index + 2].nil? == false
+        @total_score += @frames[frame_index + 2].rolls[0]
+      end
     end
   end
 end
