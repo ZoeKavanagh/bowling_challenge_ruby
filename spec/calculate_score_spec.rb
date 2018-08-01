eeffea5fd1fd9a8f30b6ca2a467666fd66429382
Zoe Kavanagh <zoekavanagh2017@hotmail.com>
2018-08-01 12:29:42 +0100

    Amdend tests to refelect removal of method for perfect_game?

--- spec/calculate_score_spec.rb
+++ spec/calculate_score_spec.rb
@@ -1,21 +1,21 @@
 require 'calculate_score'
 
 describe Calculate_score do
-  describe '#total_score' do
+  describe '#score_calc' do
     it 'will return the total score of a game with no spares or strikes' do
       simple_frame = instance_double('simple_frame', :rolls => [1, 2], :type => 'complete')
       frames = Array.new(10, simple_frame)
 
-      expect(subject.total_score(frames)).to eq 30
+      expect(subject.score_calc(frames)).to eq 30
     end
 
-    xit 'will return the total score of a game with one spare' do
+    it 'will return the total score of a game with one spare' do
       simple_frame = instance_double('simple_frame', :rolls => [1, 2], :type => 'complete')
       spare_frame = instance_double('spare_frame', :rolls => [9, 1], :type => 'spare')
       frames = [spare_frame]
       9.times { frames << simple_frame }
 
-      expect(subject.total_score(frames)).to eq 38
+      expect(subject.score_calc(frames)).to eq 38
     end
 
     it 'will return the total score of 135 of a game with all spares' do
@@ -23,24 +23,24 @@ describe Calculate_score do
       frames = []
       10.times { frames << spare_frame }
 
-      expect(subject.total_score(frames)).to eq 181
+      expect(subject.score_calc(frames)).to eq 181
     end
 
-    xit 'will return the total score of a game with one strike' do
+    it 'will return the total score of a game with one strike' do
       simple_frame = instance_double('simple_frame', :rolls => [1, 2], :type => 'complete')
       strike_frame = instance_double('strike_frame', :rolls => [10], :type => 'strike')
       frames = [strike_frame]
       9.times { frames << simple_frame }
 
-      expect(subject.total_score(frames)).to eq 40
+      expect(subject.score_calc(frames)).to eq 40
     end
 
-    it 'will return the total score of 300 for a perfect game' do
+    xit 'will return the total score of 300 for a perfect game' do
       strike_frame = instance_double('strike_frame', :rolls => [10], :type => 'strike')
       frames = []
       10.times { frames << strike_frame }
 
-      expect(subject.total_score(frames)).to eq 300
+      expect(subject.score_calc(frames)).to eq 300
     end
   end
 end
