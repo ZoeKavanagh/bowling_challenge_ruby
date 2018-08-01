67df51665d17c980560f3a7334516b12b04b591c
Zoe Kavanagh <zoekavanagh2017@hotmail.com>
2018-08-01 15:42:27 +0100

    Addional test for perfect game. Include mock for final frame

--- spec/calculate_score_spec.rb
+++ spec/calculate_score_spec.rb
@@ -3,44 +3,46 @@ require 'calculate_score'
 describe Calculate_score do
   describe '#score_calc' do
     it 'will return the total score of a game with no spares or strikes' do
-      simple_frame = instance_double('simple_frame', :rolls => [1, 2], :type => 'complete')
+      simple_frame = instance_double('simple_frame', :rolls => [1, 2], :status => 'complete')
       frames = Array.new(10, simple_frame)
 
-      expect(subject.score_calc(frames)).to eq 30
+      expect(subject.calculation(frames)).to eq 30
     end
 
     it 'will return the total score of a game with one spare' do
-      simple_frame = instance_double('simple_frame', :rolls => [1, 2], :type => 'complete')
-      spare_frame = instance_double('spare_frame', :rolls => [9, 1], :type => 'spare')
+      simple_frame = instance_double('simple_frame', :rolls => [1, 2], :status => 'complete')
+      spare_frame = instance_double('spare_frame', :rolls => [9, 1], :status => 'spare')
       frames = [spare_frame]
       9.times { frames << simple_frame }
 
-      expect(subject.score_calc(frames)).to eq 38
+      expect(subject.calculation(frames)).to eq 38
     end
 
     it 'will return the total score of 135 of a game with all spares' do
-      spare_frame = instance_double('spare_frame', :rolls => [9, 1], :type => 'spare')
+      spare_frame = instance_double('spare_frame', :rolls => [9, 1], :status => 'spare')
       frames = []
       10.times { frames << spare_frame }
 
-      expect(subject.score_calc(frames)).to eq 181
+      expect(subject.calculation(frames)).to eq 181
     end
 
     it 'will return the total score of a game with one strike' do
-      simple_frame = instance_double('simple_frame', :rolls => [1, 2], :type => 'complete')
-      strike_frame = instance_double('strike_frame', :rolls => [10], :type => 'strike')
+      simple_frame = instance_double('simple_frame', :rolls => [1, 2], :status => 'complete')
+      strike_frame = instance_double('strike_frame', :rolls => [10], :status => 'strike')
       frames = [strike_frame]
       9.times { frames << simple_frame }
 
-      expect(subject.score_calc(frames)).to eq 40
+      expect(subject.calculation(frames)).to eq 40
     end
 
-    xit 'will return the total score of 300 for a perfect game' do
-      strike_frame = instance_double('strike_frame', :rolls => [10], :type => 'strike')
+    it 'will return the total score of 300 for a perfect game' do
+      strike_frame = instance_double('strike_frame', :rolls => [10], :status => 'strike')
+      final_frame = instance_double('final_frame', :rolls => [10, 10, 10], :status => 'strike')
       frames = []
-      10.times { frames << strike_frame }
+      9.times { frames << strike_frame }
+      frames << final_frame
 
-      expect(subject.score_calc(frames)).to eq 300
+      expect(subject.calculation(frames)).to eq 300
     end
   end
 end
