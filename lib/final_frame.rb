53be7315ad17f6ade343ce74c4ce70afc43b7403
Zoe Kavanagh <zoekavanagh2017@hotmail.com>
2018-08-04 12:30:08 +0100

    Remove initialize method from FinalFrame class, create final_spare method to calculate if final frames contains a spare

--- lib/final_frame.rb
+++ lib/final_frame.rb
@@ -2,16 +2,21 @@ class FinalFrame < Frame
 
   NUMBER_OF_ROLLS = 2
 
-  def initilaize
-    @rolls = []
-  end
-
   def complete?
-    return @rolls.length > NUMBER_OF_ROLLS if strike? || spare?
+    return @rolls.length > NUMBER_OF_ROLLS if strike? || final_spare?
     @rolls.length == NUMBER_OF_ROLLS
   end
 
   def final?
     true
+  end
+
+  private
+
+  def final_spare?
+    if @rolls.length >= NUMBER_OF_ROLLS
+      rolls_sum = (@rolls.inject(0, :+))
+      rolls_sum >= 10
+    end
   end
 end
